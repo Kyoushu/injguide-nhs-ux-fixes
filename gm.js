0f17a62cd057ad1b0eb25a560ec6ad8bf7afd901
James Willans <james.willans@accordmarketing.com>
2017-01-02 18:58:59 +0000

    Improving UX

--- gm.js
+++ gm.js
@@ -8,6 +8,8 @@
 // ==/UserScript==
 (function(){
 
+    if(window.location.href.match(/\.pdf/i)) return;
+
     var script = document.createElement('script');
     script.src = 'https://rawgit.com/Kyoushu/injguide-nhs-ux-fixes/master/dist/js/app.min.js?_' + (new Date());
     document.body.appendChild(script);
