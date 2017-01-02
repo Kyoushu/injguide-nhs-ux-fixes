7329f1f607c6523af5257abd7c97305830b47f40
James Willans <james.willans@accordmarketing.com>
2017-01-02 18:12:41 +0000

    Further work on improving UX

---
+++ gm.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         injguide-nhs-ux-fixes
+// @namespace    injguide
+// @version      0.1
+// @match        http://www.injguide.nhs.uk/*
+// @match        http://medusa.wales.nhs.uk/*
+// @grant        none
+// ==/UserScript==
+(function(){
+
+    var script = document.createElement('script');
+    script.src = 'https://rawgit.com/Kyoushu/injguide-nhs-ux-fixes/master/dist/js/app.min.js?_' + (new Date());
+    document.body.appendChild(script);
+
+    var styles = document.createElement('link');
+    styles.rel = 'stylesheet';
+    styles.type = 'text/css';
+    styles.href = 'https://rawgit.com/Kyoushu/injguide-nhs-ux-fixes/master/dist/css/app.min.css?_' + (new Date());
+    document.head.appendChild(styles);
+
+})();
